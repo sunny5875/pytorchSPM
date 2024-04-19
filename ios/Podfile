e2478cd7eed22319b68484e32542637f807f737c
hyun subin <sunny5875@hanyang.ac.kr>
2024-04-19 14:14:21 +0900

    [Feat]: Change UI to SwiftUI

---
+++ ios/Podfile
@@ -0,0 +1,4 @@
+platform :ios, '12.0'
+target 'PytorchPractice' do
+    pod 'LibTorch-Lite-Nightly'
+end
--- ios/PytorchPractice/ContentView.swift
+++ ios/PytorchPractice/ContentView.swift
@@ -6,63 +6,60 @@
 //
 
 import SwiftUI
-import LibTorch
 
 
 struct ContentView: View {
-    @State var result: String = "sdsds"
+    @State var result: String = "진행중"
     
-    let image =  UIImage(named: "image.png")
+    
+    @State var image = UIImage(named: "image")
     
     var body: some View {
+        
         VStack {
-            Image("image.png", bundle: .none)
+            Image("image")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text(result)
         }
         .onAppear {
-            onAppear()
+            predict()
         }
         .padding()
     }
     
-//    private lazy var module: TorchModule = {
-//        if let filePath = Bundle.main.path(forResource: "mobilenetv2_coreml", ofType: "ptl"),
-//            let module = TorchModule(fileAtPath: filePath) {
-//            return module
-//        } else {
-//            fatalError("Can't find the model file!")
-//        }
-//    }()
-//
-//    private lazy var labels: [String] = {
-//        if let filePath = Bundle.main.path(forResource: "words", ofType: "txt"),
-//            let labels = try? String(contentsOfFile: filePath) {
-//            return labels.components(separatedBy: .newlines)
-//        } else {
-//            fatalError("Can't find the text file!")
-//        }
-//    }()
+    @State private var module: TorchModule = {
+        if let filePath = Bundle.main.path(forResource: "mobilenetv2_coreml", ofType: "ptl"),
+            let module = TorchModule(fileAtPath: filePath) {
+            return module
+        } else {
+            fatalError("Can't find the model file!")
+        }
+    }()
 
-    mutating func onAppear() {
-//        let resizedImage = image.resized(to: CGSize(width: 224, height: 224))
-//        guard var pixelBuffer = resizedImage.normalized() else {
-//            return
-//        }
-//        guard let outputs = module.predict(image: UnsafeMutableRawPointer(&pixelBuffer)) else {
-//            return
-//        }
-//        let zippedResults = zip(labels.indices, outputs)
-//        let sortedResults = zippedResults.sorted { $0.1.floatValue > $1.1.floatValue }.prefix(3)
-//        var text = ""
-//        for result in sortedResults {
-//            text += "\u{2022} \(labels[result.0]) \n\n"
-//        }
-//        result = text
+    @State private var labels: [String] = {
+        if let filePath = Bundle.main.path(forResource: "words", ofType: "txt"),
+            let labels = try? String(contentsOfFile: filePath) {
+            return labels.components(separatedBy: .newlines)
+        } else {
+            fatalError("Can't find the text file!")
+        }
+    }()
+
+    func predict() {
+        let resizedImage = image!.resized(to: CGSize(width: 224, height: 224))
+        guard var pixelBuffer = resizedImage.normalized() else {
+            return
+        }
+        guard let outputs = module.predict(image: UnsafeMutableRawPointer(&pixelBuffer)) else {
+            return
+        }
+        let zippedResults = zip(labels.indices, outputs)
+        let sortedResults = zippedResults.sorted { $0.1.floatValue > $1.1.floatValue }.prefix(3)
+        var text = ""
+        for result in sortedResults {
+            text += "\u{2022} \(labels[result.0]) \n\n"
+        }
+        result = text
     }
-}
-
-#Preview {
-    ContentView()
 }
